7d8e3474a013b7410622656521808b6c99a692ad
user <muhammed.awad@outlook.com>
2017-10-04 10:51:57 +0300

    guide

--- app/src/main/java/com/awad/hamudi/muhammadshoppinglist/LogIn.java
+++ app/src/main/java/com/awad/hamudi/muhammadshoppinglist/LogIn.java
@@ -2,12 +2,26 @@ package com.awad.hamudi.muhammadshoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity
+{
+    private EditText etEm;
+    private EditText etP;
+    private Button btin;
+    private Button btForgot;
+    private  Button btUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        etEm = (EditText) findViewById(R.id.etEm);
+        etP = (EditText) findViewById(R.id.etP);
+        btin = (Button) findViewById(R.id.btin);
+        btForgot = (Button) findViewById(R.id.btForgot);
+        btUp = (Button) findViewById(R.id.btUp);
     }
 }
