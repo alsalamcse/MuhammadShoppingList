612c55b4cb5ddfddf916286bc06889541bcfe8cb
user <muhammed.awad@outlook.com>
2017-10-30 09:44:13 +0200

    guide

--- app/src/main/java/com/awad/hamudi/muhammadshoppinglist/AddItemActivity.java
+++ app/src/main/java/com/awad/hamudi/muhammadshoppinglist/AddItemActivity.java
@@ -2,12 +2,36 @@ package com.awad.hamudi.muhammadshoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class AddItemActivity extends AppCompatActivity {
-
+public class AddItemActivity extends AppCompatActivity
+{
+    private EditText etamount;
+    private EditText etName;
+    private EditText etPrice;
+    private EditText etUnits;
+    private ImageButton iBtnImage;
+    private Button btnSave;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        etamount = (EditText) findViewById(R.id.etamount);
+        etName = (EditText) findViewById(R.id.etName);
+        etPrice = (EditText) findViewById(R.id.etPrice);
+        etUnits = (EditText) findViewById(R.id.etUnits);
+        iBtnImage = (ImageButton) findViewById(R.id.iBtnImage);
+        btnSave = (Button) findViewById(R.id.btnSave);
+    }
+    public void dataHandler(){
+        String stname= etName.getText().toString();
+        String stAmount= etamount.getText().toString();
+        String stPrice= etPrice.getText().toString();
+        String stUnits= etUnits.getText().toString();
+        double amount= Double.parseDouble(stAmount);
+        double price = Double.parseDouble(stPrice);
     }
 }
